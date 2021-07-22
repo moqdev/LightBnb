ba45d090a71279feac57d08dbca4aa520ce1f99e
Mona Waseem <monaqwaseem@gmail.com>
2021-07-22 08:33:14 +0000

    Update lightbnb Reservations functions with SQL queries.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -72,7 +72,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const values = [guest_id, limit];
+  const queryString = `SELECT 
+                       properties.*,
+                       reservations.*,
+                       avg(property_reviews.rating) AS average_rating
+                       FROM reservations
+                       LEFT JOIN properties ON properties.id = reservations.property_id
+                       LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+                       WHERE reservations.guest_id = $1 AND end_date < now()::date
+                       GROUP BY properties.id, reservations.id
+                       ORDER BY reservations.start_date
+                       LIMIT $2;`;
+ 
+  // return getAllProperties(null, 2);
 };
 exports.getAllReservations = getAllReservations;
 
